c943c032408c271f20554e7b3baf64bf32a630ae
Xylon Rad <133812267+thirdeyenation@users.noreply.github.com>
2024-09-24 21:16:05 -0400

    Turned off the model selection in initialize.py

--- initialize.py
+++ initialize.py
@@ -4,20 +4,20 @@ from agent import AgentConfig
 def initialize():
     
     # main chat model used by agents (smarter, more accurate)
-    chat_llm = models.get_openai_chat(model_name="o1-preview-2024-09-12", temperature=0.2)
-    chat_llm = models.get_ollama_chat(model_name="mattshumer/reflection-70b", temperature=0.3)
-    chat_llm = models.get_lmstudio_chat(model_name="mattshumer/reflection-70b", temperature=0.1)
-    # chat_llm = models.get_openrouter(model_name="meta-llama/llama-3-8b-instruct:free")
-    # chat_llm = models.get_azure_openai_chat(deployment_name="gpt-4o-mini", temperature=0)
-    chat_llm = models.get_anthropic_chat(model_name="claude-3-5-sonnet-20240620", temperature=0.2)
-    chat_llm = models.get_google_chat(model_name="gemini-1.5-flash", temperature=0.1)
-    # chat_llm = models.get_groq_chat(model_name="llama-3.1-70b-versatile", temperature=0)
+    #chat_llm = models.get_openai_chat(model_name="o1-preview-2024-09-12", temperature=0.2)
+    #chat_llm = models.get_ollama_chat(model_name="mattshumer/reflection-70b", temperature=0.3)
+    #chat_llm = models.get_lmstudio_chat(model_name="mattshumer/reflection-70b", temperature=0.1)
+    #chat_llm = models.get_openrouter(model_name="meta-llama/llama-3-8b-instruct:free")
+    #chat_llm = models.get_azure_openai_chat(deployment_name="gpt-4o-mini", temperature=0)
+    #chat_llm = models.get_anthropic_chat(model_name="claude-3-5-sonnet-20240620", temperature=0.2)
+    #chat_llm = models.get_google_chat(model_name="gemini-1.5-flash", temperature=0.1)
+    #chat_llm = models.get_groq_chat(model_name="llama-3.1-70b-versatile", temperature=0)
     
     # utility model used for helper functions (cheaper, faster)
     utility_llm = chat_llm # change if you want to use a different utility model
 
     # embedding model used for memory
-    embedding_llm = models.get_openai_embedding(model_name="text-embedding-3-small")
+    #embedding_llm = models.get_openai_embedding(model_name="text-embedding-3-small")
     # embedding_llm = models.get_ollama_embedding(model_name="nomic-embed-text")
     # embedding_llm = models.get_huggingface_embedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
